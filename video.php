ef6d9b5b1714857ff926256865c3d1e19ed99737
longsir0 <longsir0@users.noreply.github.com>
2020-09-05 17:48:12 +0800

    Update video.php

--- video.php
+++ video.php
@@ -1,96 +1,124 @@
 <?php
-/*$img = 'D:\video_img\video_picture\aaa.jpg';
-$str = 'ffmpeg -i D:\video_img\video\20180810\V55555.mp4 -y -f mjpeg -ss 5 -t 1 -vf scale=iw*1:ih*1 '.$img;
-system($str, $status);
-if($status == 0){
-    compressed_image($img, $img);
-}
-die;*/
-ini_set('memory_limit','3072M');    // 临时设置最大内存占用为3G
+ini_set('memory_limit','3072M');//临时设置最大内存占用为3G
 set_time_limit(0);
 
-$file = read_all("D:/video_img/video/20180810");
-for ($i=0; $i < count($file); $i++) {
-    //echo $file[$i].'<br>';
-    $str = str_replace("D:/video_img/video/", "video_picture/", $file[$i]);
-    $str = str_replace('\\', "/", $str);
-    echo $str.'<br>';
-    //getVideoCover($file[$i]);
-}
-die;
-//获得视频文件的缩略图
-//默认截取第一秒第一帧
-function getVideoCover($file, $time = 1) {
-    //生成封面图，UPLOAD_PATH绝对路径
-    $path = get_image_mp4_path();
-    $str = 'ffmpeg -i '.$file.' -y -f mjpeg -ss 5 -t 1 -vf scale=iw*1:ih*1 '.$path['img_file'];
-    system($str, $status);
-    if($status == 0){
-        //压缩
-        compressed_image($path['img_file'], $path['img_file']);
-        //视频重命名
-        rename($file, $path['mp4_file']);
-        //echo 1122;die;
-        /*$res['status'] = 'success';
-        $res['image_path'] = $image_path;*/
-    }
-    //return $res;
-}
+class Video {
+  public function __construct() {}
 
-//创建图片名称
-function get_image_mp4_path(){
-    $name = md5(date('YmdHis', time().rand(1000, 9999)));
-    $res['img_file'] = video_mkdir().'/'.$name.".jpg";
-    $res['mp4_file'] = video_mkdir('video').'/'.$name.".mp4";
-    return $res;
-}
-
-//判断文件夹是否存在，不存在则创建文件夹
-function video_mkdir($name = 'video_picture'){
-    $path = 'D:\video_img\/'.$name.'/';
-    if (!file_exists($path)){
-        mkdir($path);
+  /**
+   * 开始遍历目录下的视频
+   *
+   * @access public
+   * @param  id 产品ID
+   * @param  titles 产品标题
+   * @param  grade 二维码背景图
+   * @return string
+   */
+  public function run($file_path) {
+    $file_path = self::read_all("/www/wwwroot/Test/video");
+    for ($i = 0; $i < count($file_path); $i++) {
+        //$str = str_replace("/www/wwwroot/Test/", "video_picture/", $file_path[$i]);
+        //$str = str_replace('\\', "/", $str);
+        self::getVideoCover($file_path[$i]);
     }
-    $path .= date("Ymd");
-    if (!file_exists($path)){
-        mkdir($path); 
-    }
-    return 'D:/video_img/'.$name.'/'.date("Ymd");
-}
-//读取文件夹下所有文件
-function read_all ($dir){
-    $file_arr = [];
-    if(!is_dir($dir)) return false;
-    $handle = opendir($dir);
-    if($handle){
-        while(($fl = readdir($handle)) !== false){
-            $temp = $dir.DIRECTORY_SEPARATOR.$fl;
-            //如果不加  $fl!='.' && $fl != '..'  则会造成把$dir的父级目录也读取出来
-            if(is_dir($temp) && $fl!='.' && $fl != '..'){
-               /*//递归，文件夹内包含文件的情况
-               echo '目录：'.$temp.'<br>';
-               read_all($temp);
-               */
-            }else{
-                if($fl!='.' && $fl != '..'){
-                    $file_arr[] = $temp;
-                }
-            }
-        }
-   }
-   return $file_arr;
-}
+    return 'success';
+  }
 
-/**
+  /**
+   * 获得视频文件的缩略图
+   *
+   * @access public
+   * @param  file 视频路径
+   * @param  time 截取位置，默认第一秒
+   * @return string
+   */
+  private function getVideoCover($file, $time = 1) {
+      //生成封面图，UPLOAD_PATH绝对路径
+      $path = self::get_image_mp4_path();
+      $str = 'ffmpeg -i '.$file.' -y -f mjpeg -ss 5 -t 1 -vf scale=iw*1:ih*1 '.$path['img_file'];
+      echo $str;die;
+      $a = system($str, $status);
+      if($status == 0){
+          //压缩
+          self::compressed_image($path['img_file'], $path['img_file']);
+          //视频重命名
+          rename($file, $path['mp4_file']);
+      }
+      return;
+  }
+
+  /**
+   * 创建图片名称
+   *
+   * @access public
+   * @return array
+   */
+  public function get_image_mp4_path(){
+      $name = md5(date('YmdHis', time().rand(1000, 9999)));
+      $res['img_file'] = self::video_mkdir().'/'.$name.".jpg";
+      $res['mp4_file'] = self::video_mkdir('video').'/'.$name.".mp4";
+      return $res;
+  }
+
+  /**
+   * 判断目录是否存在，不存在则创建
+   *
+   * @access public
+   * @param  name 目录名
+   * @return string
+   */
+  public function video_mkdir($name = 'video_picture'){
+      $path = '/www/wwwroot/Test/'.$name.'/';
+      if (!file_exists($path)){
+          mkdir($path);
+      }
+      $path .= date("Ymd");
+      if (!file_exists($path)){
+          mkdir($path);
+      }
+      return '/www/wwwroot/Test/'.$name.'/'.date("Ymd");
+  }
+
+  /**
+   * 读取目录下所有文件
+   *
+   * @access public
+   * @param  name 目录名
+   * @return string
+   */
+  public function read_all($dir){
+      $file_arr = [];
+      if(!is_dir($dir)) return false;
+      $handle = opendir($dir);
+      if($handle){
+          while(($fl = readdir($handle)) !== false){
+              $temp = $dir.DIRECTORY_SEPARATOR.$fl;
+              //如果不加  $fl!='.' && $fl != '..'  则会造成把$dir的父级目录也读取出来
+              if(is_dir($temp) && $fl!='.' && $fl != '..'){
+                 /*//递归，目录内包含文件的情况
+                 echo '目录：'.$temp.'<br>';
+                 read_all($temp);
+                 */
+              }else{
+                  if($fl!='.' && $fl != '..'){
+                      $file_arr[] = $temp;
+                  }
+              }
+          }
+     }
+     return $file_arr;
+  }
+
+  /**
    * desription 判断是否gif动画
    * @param sting $image_file图片路径
    * @return boolean t 是 f 否
    */
-  function check_gifcartoon($image_file){
+  public function check_gifcartoon($image_file){
     $fp = fopen($image_file,'rb');
     $image_head = fread($fp,1024);
     fclose($fp);
-    return preg_match("/".chr(0x21).chr(0xff).chr(0x0b).'NETSCAPE2.0'."/",$image_head)?false:true;
+    return preg_match("/".chr(0x21).chr(0xff).chr(0x0b).'NETSCAPE2.0'."/",$image_head) ? false : true;
   }
  
   /**
@@ -98,15 +126,15 @@ function read_all ($dir){
   * @param sting $imgsrc 图片路径
   * @param string $imgdst 压缩后保存路径
   */
-  function compressed_image($imgsrc,$imgdst){
-    list($width,$height,$type)=getimagesize($imgsrc);
+  public function compressed_image($imgsrc,$imgdst){
+    list($width,$height,$type) = getimagesize($imgsrc);
     /*$new_width = ($width>600? $width*0.5 : $width);
     $new_height = ($height>600 ? $height*0.5 : $height);*/
     $new_width = $width;
     $new_height = $height;
     switch($type){
       case 1:
-        $giftype=check_gifcartoon($imgsrc);
+        $giftype = self::check_gifcartoon($imgsrc);
         if($giftype){
           header('Content-Type:image/gif');
           $image_wp=imagecreatetruecolor($new_width, $new_height);
@@ -137,3 +165,9 @@ function read_all ($dir){
         break;
     }
   }
+}
+/************************Test**********************************/
+$obj = new Video();
+$res = $obj->run("/www/wwwroot/Test/video_img");
+var_dump($res);die;
+/************************Test End**********************************/
